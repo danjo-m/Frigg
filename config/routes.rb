e6fa03ef64788626ef5249cfa07fa60d1f87bed9
Miku Danjo <339mimic@gmail.com>
2017-04-28 19:18:30 +0900

    Redirect steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: redirect('/steps')
   resources :steps
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
